934690f51b2357e147bd941af73c705592139bee
anvishetty14 <anvishetty14@gmail.com>
2026-06-18 18:54:08 +0530

    Added verification and reputation system

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { status } = require("express/lib/response");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
@@ -9,12 +10,23 @@ let posts = [
   {
     id: 1,
     title: "My First Post",
-    likes: 10
+    likes: 10,
+    status: "pending verification"
   },
   {
     id: 2,
     title: "Learning Express",
-    likes: 25
+    likes: 25,
+    status: "pending verification"
+  }
+];
+
+let users = [
+  {
+    id: 1,
+    name: "Anvii",
+    curatorScore: 0,
+    contributorScore: 0
   }
 ];
 
@@ -40,7 +52,10 @@ app.get("/posts/:id", (req, res) => {
 
 // POST new post
 app.post("/posts", (req, res) => {
-  const newPost = req.body;
+  const newPost = {
+     ...req.body,
+     status: "pending verification"
+  };
 
   posts.push(newPost);
 
@@ -99,6 +114,47 @@ app.delete("/posts/:id", (req, res) => {
     //console.log("MongoDB Error:", err);
 //});
 // Start server
+// VERIFY POST
+
+app.put("/posts/:id/verify", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const post = posts.find(p => p.id === id);
+
+  if (!post) {
+    return res.status(404).json({
+      message: "Post not found"
+    });
+  }
+
+  post.status = "verified";
+
+  res.json({
+    message: "Post verified successfully",
+    post
+  });
+});
+app.get("/users/:id/reputation", (req, res) => {
+  const user = users.find(
+    u => u.id === parseInt(req.params.id)
+  );
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found"
+    });
+  }
+
+  const totalLikes = posts.reduce(
+    (sum, post) => sum + post.likes,
+    0
+  );
+
+  user.contributorScore = totalLikes;
+  user.curatorScore = Math.floor(totalLikes / 2);
+
+  res.json(user);
+});
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
